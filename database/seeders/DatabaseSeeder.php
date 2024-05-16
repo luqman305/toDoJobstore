055fef9d39fb9432f7a8a4b434f0312fe130570a
Luqman Arif <luqman305@gmail.com>
2024-05-16 15:44:50 +0800

    remove unused model,seeder,factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,9 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            UserSeeder::class,
-            TaskSeeder::class,
-        ]);
+        //
     }
 }
